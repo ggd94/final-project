236421f234d727f53273487a276377448d10aa07
Giulia <giuliagiugno94@gmail.com>
2016-10-02 12:20:38 +0000

    first introduction of stacic-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #you have to add this if you want a function run root 'controller_name#action_name'
-  root 'users#index'
+  root 'application#hello'
 end
