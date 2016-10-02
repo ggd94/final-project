9698267f78e666fe5246c48920649ad4aefc43b3
Giulia <giuliagiugno94@gmail.com>
2016-10-02 13:24:50 +0000

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
+  get  'static_pages/about'
   get 'static_pages/help'
 
   resources :microposts
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #you have to add this if you want a function run root 'controller_name#action_name'
-  root 'application#hello'
+  root 'static_pages#home'
 end
