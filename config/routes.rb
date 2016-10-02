f714b28eb64c026ae1f444ef4bd4c099a72183b7
Giulia <giuliagiugno94@gmail.com>
2016-10-02 10:38:35 +0000

    new users and microposts resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #you have to add this if you want a function run root 'controller_name#action_name'
-  root 'application#hello'
+  root 'users#index'
 end
