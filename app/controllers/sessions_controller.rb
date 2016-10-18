4df74fc850ccb23e28a4046e01cce1774cd28939
Giulia <giuliagiugno94@gmail.com>
2016-10-18 14:15:16 +0000

    All the cookies part for the login is done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,13 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      log_in user #chiama il session helper! posso chiamarlo perche l ho incluso in appl. controller
+      log_in user#chiama il session helper! posso chiamarlo perche l ho incluso in appl. controller
+      remember user
+      if params[:session][:remember_me] == '1'
+        remember(user)
+      else
+        forget(user)
+      end
       redirect_to user
     else
       # Create an error message.
@@ -16,7 +22,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   before_save { self.email = email.downcase }
   
   has_secure_password
@@ -15,4 +17,23 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
   
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token =User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    if remember_digest.nil?
+      false
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  #si limita ad eliminare user remember e quindi ad eliminare eventuali cookies
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
